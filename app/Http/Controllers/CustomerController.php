418d736e347b4b760096bff4d07a9a0fe7b29fe9
DishikaVeruwantha <veruwanthadishika@gmail.com>
2025-06-27 18:02:10 +0530

    created the basic functions for crud operations

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class CustomerController extends Controller
 {
-    //
+    public function edit(Customer $customer): Response
+    {
+        return Inertia::render('Customers/Edit', [
+            'customer' => $customer
+        ]);
+    }
+
+    public function update(Request $request, Customer $customer)
+    {
+        $data = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:customers,email,' . $customer->id,
+            'invoice' => 'boolean',
+        ]);
+
+        $customer->update($data);
+
+        return redirect()->route('profile.customerEdit')->with('success', 'Customer updated.');
+    }
+
+    public function destroy(Customer $customer)
+    {
+        $customer->delete();
+
+        return redirect()->route('/customers')->with('success', 'Customer deleted.');
+    }
+
 }
