a3a043b4a8a4c0aaa52184fc3c8e536e3dfb90ca
DishikaVeruwantha <veruwanthadishika@gmail.com>
2025-06-27 17:50:23 +0530

    add a created date timestamp to customer table

--- database/migrations/2014_10_12_2000000_create_customers_table.php
+++ database/migrations/2014_10_12_2000000_create_customers_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamps();
+            $table->timestamp('user_created_at')->nullable();
             $table->boolean(column:'invoice')->default(false); // Active status
         });
     }
