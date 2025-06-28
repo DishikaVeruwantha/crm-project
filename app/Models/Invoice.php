64d91fbb49b34cc8d3b3f48e34bf312538afc8fc
DishikaVeruwantha <veruwanthadishika@gmail.com>
2025-06-28 14:51:19 +0530

    created the invoice module

---
+++ app/Models/Invoice.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Invoice extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'customer_id',
+        'amount',
+        'due_date',
+        'status',
+        'sent',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'amount' => 'decimal:2',
+        'due_date' => 'date',
+        'sent' => 'boolean',
+    ];
+
+    // Relationship
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+}
