a689e15bc0e4c378dd5b5901ae34961e45db1406
DishikaVeruwantha <veruwanthadishika@gmail.com>
2025-06-29 22:46:52 +0530

    created the transaction module

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'invoice_id',
+        'amount',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'amount' => 'decimal:2',
+    ];
+
+    // Relationship
+    public function invoice()
+    {
+        return $this->belongsTo(Invoice::class);
+    }
 }
