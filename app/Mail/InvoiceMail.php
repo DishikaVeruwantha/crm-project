ad6638cf0850b9010fb916d98fa4201f4075ed06
DishikaVeruwantha <veruwanthadishika@gmail.com>
2025-06-29 11:16:46 +0530

    updated the email subject and add the content template path to the invoice controller

--- app/Mail/InvoiceMail.php
+++ app/Mail/InvoiceMail.php
@@ -5,20 +5,23 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Attachment;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
+use App\Models\Invoice;
 
 class InvoiceMail extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public Invoice $invoice;
     /**
      * Create a new message instance.
      */
-    public function __construct()
+    public function __construct($invoice)
     {
-        //
+        $this->invoice = $invoice;
     }
 
     /**
@@ -37,14 +40,15 @@ class InvoiceMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'emails.invoice',
+            with: ['invoice' => $this->invoice]
         );
     }
 
     /**
      * Get the attachments for the message.
      *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     * @return array<int, Attachment>
      */
     public function attachments(): array
     {
